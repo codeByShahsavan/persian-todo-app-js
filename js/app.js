52b1bed07b103f6e2435879c99824f869d820b15
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-09-22 20:31:51 +0330

    persian-todo-app

---
+++ js/app.js
@@ -0,0 +1,196 @@
+
+
+const  taskInput=document.getElementById("task-input")
+const  dateInput=document.getElementById("date-input")
+const  addButton=document.getElementById("add-button")
+const  alertMessage=document.getElementById("alert-message")
+const todosBody=document.querySelector("tbody")
+const  deleteAllButton=document.getElementById("delete-all-button")
+const editButton=document.getElementById("edit-button")
+const filterButtons=document.querySelectorAll(".filter-todos")
+
+
+let todos=JSON.parse(localStorage.getItem("todos"))||[]
+
+const saveToLocalStorage=()=>{
+localStorage.setItem("todos",JSON.stringify(todos))
+
+}
+const generateId=()=>{
+     return Math.round(Math.random()*Math.random()*Math.pow(10,15)).toString()
+
+}
+
+
+
+const showAlert=(message,type)=>{
+    alertMessage.innerHTML=""
+    const alert=document.createElement("p")
+    alert.innerText=message
+    alert.classList.add("alert")
+    alert.classList.add(`alert-${type}`)
+    alertMessage.append(alert)
+    setTimeout(()=>{
+        alert.style.display="none"
+    },2000)
+}
+
+
+ const displayTodos=(data)=>{
+const todoList=data||todos
+
+    todosBody.innerHTML=""
+  if(!todoList.length){
+    todosBody.innerHTML="<tr><td colspan='4'>No task found!</td></tr>"
+    return;
+  }
+  todoList.forEach(todo => {
+
+    todosBody.innerHTML+=
+    `<tr>
+    <td>${todo.task}</td>
+    <td>${todo.date||"No Date"}</td>
+    <td>${todo.completed?"Completed":"Pending"}</td>
+    <td>
+    <button onClick="editHandler('${todo.id}')">Edit</button>
+    <button onClick="toggleHandler('${todo.id}')">${todo.completed?"Undo":"Do"}</button>
+    <button onClick="deleteHandler('${todo.id}')">Delete</button>
+
+    </td>
+    
+    </tr>
+    `
+  });
+   
+ }
+const addHandler=()=>{
+    const task=taskInput.value
+    const date=dateInput.value
+    const todo={
+        id:generateId(),
+        completed:false,
+        task,
+        date,
+
+
+    }
+    if(task){
+      todos.push(todo)
+      saveToLocalStorage()
+     displayTodos()
+ 
+
+      taskInput.value=""
+      dateInput.value=""
+      console.log(todos)
+      showAlert("Todo added successfully","success")
+
+    }
+    else{
+        showAlert("Please enter a todo!","error")
+    }
+}
+
+
+const deleteAllHandler=()=>{
+    if(todos.length){
+        todos=[]
+        saveToLocalStorage()
+        displayTodos()
+        showAlert("All todos cleared successfully","success")
+    }else{
+        showAlert("no todos to clear","error")
+        
+    }
+    
+}
+
+
+const deleteHandler=(id)=>{
+const newTodos=todos.filter((todo)=>todo.id!==id)
+  todos=newTodos
+  saveToLocalStorage()
+  displayTodos()
+  showAlert("todo deleted successfully","success")
+}
+
+
+const toggleHandler=id=>{
+//    todos.forEach((todo)=>{
+//    if(todo.id===id){
+//     todo.completed=!todo.completed
+//    }
+//    })
+//////////
+// const newTodos=todos.map((todo)=>{
+//     if(todo.id===id){
+//         return{
+//             // id:todo.id,
+//             // task:todo.task,
+//             // date:todo.date,
+//             ...todo,
+//             completed:!todo.completed
+//         }
+//     }else{
+//        return todo
+//     }
+// })
+// todos=newTodos
+////////
+const todo=todos.find((todo)=>todo.id===id)
+todo.completed=!todo.completed
+saveToLocalStorage()
+displayTodos()
+showAlert("Todo status successfully","success")
+}
+
+
+const editHandler=(id)=>{
+const todo=todos.find((todo)=>todo.id===id)
+taskInput.value=todo.task
+dateInput.value=todo.date
+editButton.style.display="inline-block"
+addButton.style.display="none"
+editButton.dataset.id=id
+}
+ 
+const applyEditHandler=event=>{
+ const id=event.target.dataset.id
+ const todo=todos.find((todo)=>todo.id===id)
+ todo.task=taskInput.value
+ todo.date=dateInput.value
+ taskInput.value=""
+ dateInput.value=""
+addButton.style.display="inline-block"
+editButton.style.display="none"
+saveToLocalStorage()
+displayTodos()
+showAlert("Todo edited successfully","success")
+    
+}
+const filterHandler=event=>{
+    let filterTodos=null;
+    const filter=event.target.dataset.filter
+    switch (filter) {
+        case "pending":
+            filterTodos=todos.filter((todo)=>todo.completed===false);
+            break;
+        case "completed":
+            filterTodos=todos.filter((todo)=>todo.completed===true);
+           break;
+        default:
+            filterTodos=todos;
+            break;
+    }
+    displayTodos(filterTodos)
+}
+window.addEventListener("load",()=>displayTodos())
+addButton.addEventListener("click",addHandler)
+deleteAllButton.addEventListener("click",deleteAllHandler)
+
+editButton.addEventListener("click",applyEditHandler)
+
+
+filterButtons.forEach((button)=>{
+    button.addEventListener("click",filterHandler)
+})
